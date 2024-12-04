474c4b75e72c2e43aa6b8416c1a30f3bff7d4a83
JEmmanuelL <Emma_lopez2917@hotmail.com>
2024-12-03 22:47:05 -0600

    Reapply "CRUD Web forms y primera parte de CRUDALumnos"
    This reverts commit c0cee3a67d75567755eda1ed15dd09625732cc5d.

---
+++ ADOWebForms/ADOWebForms/Forms/Delete.aspx.cs
@@ -0,0 +1,40 @@
+﻿using ADOWebForms.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ADOWebForms.Forms
+{
+    public partial class Delete : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int id = int.Parse(Request.QueryString["id"] ?? "1"); //Verifica si el valor de request es nulo en caso verdadero pone 1
+
+            List<EstatusAlumno> ListaEstatus = new List<EstatusAlumno>();
+
+            ADOEstatusAlumno onjCRUD = new ADOEstatusAlumno();
+
+            EstatusAlumno estatus = onjCRUD.Consultar(id);
+
+            lblid.Text = estatus.id.ToString();
+
+            lblNombreDef.Text = estatus.nombre;
+
+            lblNombreClv.Text = estatus.clave;
+        }
+
+        protected void btnEliminar_Click(object sender, EventArgs e)
+        {
+            ADOEstatusAlumno onjCRUD = new ADOEstatusAlumno();
+            int id = int.Parse(Request.QueryString["id"] ?? "1");
+
+           onjCRUD.Eliminar(id);
+
+            Response.Redirect($"index.aspx");
+        }
+    }
+}
---
+++ ADOWebForms/ADOWebForms/Forms/Edit.aspx.designer.cs
@@ -0,0 +1,80 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace ADOWebForms.Forms
+{
+
+
+    public partial class Edit
+    {
+
+        /// <summary>
+        /// Control lblID.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblID;
+
+        /// <summary>
+        /// Control idEsta.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox idEsta;
+
+        /// <summary>
+        /// Control lblnombreTer.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblnombreTer;
+
+        /// <summary>
+        /// Control nameEsta.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox nameEsta;
+
+        /// <summary>
+        /// Control lblnombreTer2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblnombreTer2;
+
+        /// <summary>
+        /// Control clvEsta.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox clvEsta;
+
+        /// <summary>
+        /// Control btnGuardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGuardar;
+    }
+}
