33481e72f9681362b1cf42688d3c459a9b75689e
JEmmanuelL <Emma_lopez2917@hotmail.com>
2024-12-09 11:26:14 -0600

    MVC RAZOR

---
+++ 4.-MVC/Razor/Razor/Controllers/HolaMundoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Razor.Controllers
+{
+    public class HolaMundoController : Controller
+    {
+        // GET: HolaMundo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult HolaMundo()
+        {
+            return View();
+        }
+
+        public ActionResult Ciclos()
+        {
+            return View();
+        }
+
+    }
+
+
+
+}
